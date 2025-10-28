7b1d76f653997c67c6320733b28146ad72366e11
siddharth-1118 <saisiddharthvooka@gmail.com>
2025-10-28 21:36:13 +0530

    update website

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import Layout from "./pages/layout";
 import About from "./pages/about";
