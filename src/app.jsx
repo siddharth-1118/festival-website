78db210c00ec1235e151c71bdf2148bc56762268
siddharth-1118 <saisiddharthvooka@gmail.com>
2025-11-01 11:14:23 +0530

    update website

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {SpeedInsights} from "@versel/speed-insights/react";
 
 import Home from "./pages/home";
 import About from "./pages/about";
